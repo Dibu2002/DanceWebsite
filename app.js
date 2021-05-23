88af86f5a5e9eec232c9ceed3c9ab0b564db3937
Divya <81864873+Dibu2002@users.noreply.github.com>
2021-05-23 17:37:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,46 +4,46 @@ const app=express();
 const path=require('path');
 const port=80;
 
-app.use('/public',express.static("public"));
+app.use('https://dibu2002.github.io/DanceWebsite/public',express.static("public"));
 app.use(express.urlencoded());
 
 app.set('view engine','pug');
 app.set('views', path.join(__dirname,'views'));
 
-app.get('/',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/',(req,res)=>{
     res.render('home.pug')          //use render (not send)with pug
 });
 
-app.get('/services',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/services',(req,res)=>{
     res.render('services.pug')
 });
 
-app.get('/about',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/about',(req,res)=>{
     res.render('about.pug')
 });
 
-app.get('/showconfirm',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/showconfirm',(req,res)=>{
     res.render('showconfirm.pug')
 });
 
-app.get('/contact',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/contact',(req,res)=>{
     res.render('contact.pug')
 });
 
-app.get('/registration',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/registration',(req,res)=>{
     res.render('registration')
 });
 
-app.get('/read1',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/read1',(req,res)=>{
     res.render('read1.pug')
 });
 
-app.get('/read2',(req,res)=>{
+app.get('https://dibu2002.github.io/DanceWebsite/read2',(req,res)=>{
     res.render('read2.pug')
 });
 
 // read through post
-app.post('/handleForm',(req,res)=>{
+app.post('https://dibu2002.github.io/DanceWebsite/handleForm',(req,res)=>{
     let varia=req.body;
     res.render('handleForm',{'data':varia})
 });
